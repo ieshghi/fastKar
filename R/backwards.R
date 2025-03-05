99f45027a9d9beff6bdebca9acaf771f79513255
ieshghi <ieshghi@nygenome.org>
2025-03-05 16:57:16 -0500

    graph traversal works properly now. also implemented in C++ (needs to be tested)

--- R/backwards.R
+++ R/backwards.R
@@ -2,6 +2,7 @@
 
 #goes from a ggraph to a "wiring", which gives all the internal edges of the graph (going from left side of a node to right side)
 #along with the loose node ids and a reference data table with the new node ids (all copies of each node are de-duplicated)
+
 gg.to.wiring <- function(gg){ 
   nodesdt = gg$nodes$dt[,.(start,end,seqnames,snode.id,cn,loose.cn.left,loose.cn.right)]
   nodesgr = gg$nodes$gr[,c('snode.id','cn')]
@@ -20,97 +21,86 @@ gg.to.wiring <- function(gg){
   # each copy of each node has two edges coming out of it, one on the left one on the right. Each of those edges has a unique edge id which we call "subid"
   internal.edges = cbind(left.split.edgetable[,.(n,left=subid)],right.split.edgetable$subid)[,.(n,left,right=V2)][,cn:=.N,by=n] #each unique mapping of right-edged to left-edges (reorderings of the third column) corresponds to a unique walk decomposition of the graph
   # where are the loose edges?
-  loose.subids = split.edgetable[type=='LOO']$subid
+  loose.subids = split.edgetable[type=='LOO']$subid %>% unique
   #split the nodes in the graph to half-nodes. This needs to be done only one time. Then, aggregate Hi-C data to those half-nodes.
   nodesdt.split = nodesdt[cn>0][rep(1:.N,each=2)][,lr:=ifelse(mod(.I,2)==1,'l','r')][,.(start,end,seqnames,snode.id,cn,lr)][,width:=ifelse(lr=='l',floor((end-start+1)/2),ceil((end-start+1)/2))]
   nodesdt.split[lr=='l',end:=start+width-1]
   nodesdt.split[lr=='r',start:=end-width+1]
-  ref.splitnodes = nodesdt.split[,.(nodeid=paste0(abs(snode.id),lr),width,refid=.I)]
-  return(list(internal.edges,loose.subids,ref.splitnodes,gg))
+  ref.splitnodes = nodesdt.split[,.(start,end,nodeid=paste0(abs(snode.id),lr),width,refid=.I)]
+  internal.edges[,id:=.I]
+  return(list(internal.edges = internal.edges,loose.ends = loose.subids,gr.splitnodes=ref.splitnodes,gg=gg))
 }
 
-# given a wiring (a mapping of left-edges to right-edges), there should be a unique set of walks which traverse that graph.
-paths_from_edges_R <- function(iedge, jedge, nodes, num_edges, looseends) {
-  # Number of edges and rows
-  n <- num_edges
-  nrows <- length(nodes)
-  # Create adjacency list representation
-  adj_list <- vector("list", n)
-  # Fill adjacency list from edge list
-  for (k in seq_len(nrows)) {
-    u <- iedge[k]  # 1-based indexing
-    v <- jedge[k]
-    adj_list[[u]] <- append(adj_list[[u]], list(c(v, k)))
-    adj_list[[v]] <- append(adj_list[[v]], list(c(u, -k)))  # Sign to track strand
-  }
-  # Result lists to store paths and cycles
-  paths <- list()
-  cycles <- list()
-  visited_edge <- rep(FALSE, n)  # Track visited edges
-  # Function to traverse paths
-  traverse_path <- function(start) {
+# given a wiring, with internal edges and loose ends, traverse graph and return paths and cycles
+traverse_graph <- function(A, loose.ends) {
+  visited_edges <- c()
+  visited_rows <- c()
+  traversed_paths <- list()
+  # Only process loose ends that haven't been traversed
+  remaining_loose_ends <- setdiff(loose.ends, visited_edges)
+  while (length(remaining_loose_ends) > 0) {
+    start_edge <- remaining_loose_ends[1]  # Pick an unvisited loose end
     path <- c()
-    current <- start
-    visited_edge[current] <- TRUE
-    prev <- -1
-    while (TRUE) {
-      found_next <- FALSE
-      for (neighbor in adj_list[[current]]) {
-        next_edge <- neighbor[1]
-        row_id <- neighbor[2]
-
-        if (!visited_edge[next_edge]) {
-          visited_edge[next_edge] <- TRUE
-          path <- c(path, if (row_id > 0) nodes[row_id] else -nodes[-row_id])
-          prev <- current
-          current <- next_edge
-          found_next <- TRUE
-          break
-        }
+    nodepath <- c()
+    current_edge <- start_edge
+    while (!is.na(current_edge) && !(current_edge %in% remaining_loose_ends && length(path) > 0)) {
+      path <- c(path, current_edge)
+      visited_edges <- c(visited_edges, current_edge)
+      
+      # Find the row where current_edge appears in 'left' or 'right'
+      row <- A[(left == current_edge | right == current_edge) & !(id %in% visited_rows)]
+      visited_rows <- c(visited_rows,row$id)
+      # Determine the next edge
+      if (row$left == current_edge) {
+        nodepath <- c(nodepath,row$n)
+        next_edge <- row$right
+      } else {
+        nodepath <- c(nodepath,-row$n)
+        next_edge <- row$left
       }
-      if (!found_next) break  # End of path
+      # Find the next occurrence of next_edge (excluding current row)
+      next_row <- A[(left == next_edge | right == next_edge) & !(id %in% visited_rows)]
+      if (nrow(next_row) == 0){ 
+        visited_edges = c(visited_edges,next_edge) 
+        path = c(path,next_edge) 
+        break  # No more paths
+      } 
+      current_edge <- next_edge  # Move to the next edge
     }
-    return(if (length(path) > 0) path else NULL)
+    traversed_paths = append(traversed_paths,list(nodepath))
+    remaining_loose_ends <- setdiff(loose.ends, visited_edges)  # Update unvisited loose ends
   }
-  # Traverse paths from looseends
-  for (start in looseends) {
-    path <- traverse_path(start)
-    if (!is.null(path)) {
-      paths <- append(paths, list(path))
-    }
-  }
-  # Detect cycles for remaining unvisited edges
-  for (k in seq_len(nrows)) {
-    if (!visited_edge[iedge[k]]) {
-      cycle <- c()
-      current <- iedge[k]
-      visited_edge[current] <- TRUE
-      prev <- -1
-
-      while (TRUE) {
-        found_next <- FALSE
-
-        for (neighbor in adj_list[[current]]) {
-          next_edge <- neighbor[1]
-          row_id <- neighbor[2]
-
-          if (!visited_edge[next_edge]) {
-            visited_edge[next_edge] <- TRUE
-            cycle <- c(cycle, if (row_id > 0) nodes[row_id] else -nodes[-row_id])
-            prev <- current
-            current <- next_edge
-            found_next <- TRUE
-            break
-          }
-        }
-        if (!found_next || (length(cycle) > 1 && current == iedge[k])) {
-          break  # Cycle closed
-        }
+  #now we detect cycles
+  remaining_edges <- A[!(id %in% visited_rows)]
+  cycles <- list()
+  while (nrow(remaining_edges) > 0) {
+    start_edge <- remaining_edges$right[1]  # Pick an unvisited edge
+    visited_rows = c(visited_rows,remaining_edges$id[1])
+    path <- c()
+    nodepath = c(remaining_edges$n[1])
+    current_edge <- start_edge
+    while (!is.na(current_edge)) {
+      path <- c(path, current_edge)
+      # Find the row containing this edge
+      row <- remaining_edges[(left == current_edge | right == current_edge) & !(id %in% visited_rows)]
+      if (nrow(row) == 0) {
+        cycles <- append(cycles, list(nodepath))
+        break
       }
-      if (length(cycle) > 0) {
-        cycles <- append(cycles, list(cycle))
+      visited_rows <- c(visited_rows,row$id)
+      # Determine the next edge
+      if (row$left == current_edge) {
+        nodepath <- c(nodepath,row$n)
+        next_edge <- row$right
+      } else {
+        nodepath <- c(nodepath,-row$n)
+        next_edge <- row$left
       }
+      # If we revisit an edge, a cycle is found
+      current_edge <- next_edge  # Move to next edge
     }
+    # Remove visited edges from remaining_edges
+    remaining_edges <- remaining_edges[!(id %in% visited_rows)]
   }
-  return(list(paths = paths, cycles = cycles))
-}
+  return(list(paths=traversed_paths,cycles=cycles))
+} # ---> to be made into a Rcpp function for faster evaluation
