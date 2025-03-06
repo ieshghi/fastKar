7a7d6c7742ccc2023e8f63846509648377dd1bc3
ieshghi <ieshghi@nygenome.org>
2025-03-06 14:55:04 -0500

    changed order of rebin.data and pred in test.walks.with.hic (in return=='scoremaps')

--- R/comparisons.R
+++ R/comparisons.R
@@ -36,7 +36,7 @@ test.walks.with.hic <- function(walkset,hic.data,resolution=1e5,mc.cores=1,retur
         scores = mclapply(predictions,function(pred){compmaps(rebin.data,pred,ifsum=TRUE,theta=3)},mc.cores=mc.cores)
         return(scores)
     } else if (return=='scoremaps'){
-        scoremaps = mclapply(predictions,function(pred){compmaps(pred,rebin.data,ifsum=FALSE,theta=3)},mc.cores=mc.cores)
+        scoremaps = mclapply(predictions,function(pred){compmaps(rebin.data,pred,ifsum=FALSE,theta=3)},mc.cores=mc.cores)
         return(scoremaps)
     } else if (return=='predictions'){
         return(predictions)
