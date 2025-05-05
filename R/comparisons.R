8b33ba21fa11b82d6f31449731e633fb9bba0795
Stanley Clarke <stanleyrc@users.noreply.github.com>
2025-05-05 11:55:46 -0400

    adds hic.data to return sp for test.walks.with.hic

--- R/comparisons.R
+++ R/comparisons.R
@@ -44,7 +44,7 @@ test.walks.with.hic <- function(walkset,hic.data,resolution=1e5,mc.cores=1,targe
         return(predictions)
     } else if (return == 'sp') {
         scores = mclapply(predictions,function(pred){compmaps(pred,rebin.data,ifsum=TRUE,theta=3)},mc.cores=mc.cores)
-        return(list(scores = scores, predictions = predictions))
+        return(list(scores = scores, predictions = predictions, hic.data = rebin.data))
     } else if (return == 'all') {
         scores = mclapply(predictions,function(pred){compmaps(pred,rebin.data,ifsum=TRUE,theta=3)},mc.cores=mc.cores)
         scoremaps = mclapply(predictions,function(pred){compmaps(pred,rebin.data,ifsum=FALSE,theta=3)},mc.cores=mc.cores)
